915d476cceb6ee1e58268bc52c098b196f165e3e
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-08-12 07:52:31 -0400

    update print & upload

--- printForm.js
+++ printForm.js
@@ -1,36 +1,41 @@
+let id = ''
 const params = new URLSearchParams(window.location.search)
-for (const [key, value] of params) { let id = value; }
+for (const [key, value] of params) { id = value; }
 
-//Show animation 
-for (let i = 1; i < 5; i++) {
-    document.getElementById(`mainSection${1}`).style.display = "none";
+createSearchQuery('familyTrainingMeeting',id)
+
+const title = document.getElementById('pageTitle')
+const form1 = document.getElementById('formResponse1')
+const form2 = document.getElementById('formResponse2')
+const sign = document.getElementById('signature')
+const printForm = document.getElementById('printToPDF')
+
+function createSearchQuery(form, id) {
+     console.log(id, form)
+    //fetch data
+    const url = `https://pffm.azurewebsites.net/getForms/?form=${form}&key=${id}`
+    const header = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    fetch(url, {
+        method: "GET",
+        headers: header
+    })
+        .then(response => response.json())
+        .then(data => populatePage(data))
+        .catch(console.error)
+        
 }
 
-//fetch data
-const url = 'https://pffm.azurewebsites.net/getForms'
-const query = {
-    form: 'familyTrainingMeeting',
-    itemId: id 
-}
-const header = {
-    'Content-Type': 'application/json',
-    "Access-Control-Allow-Origin": "*"
-}
-
-fetch(url, {
-    method: "GET",
-    headers: header,
-    body: JSON.stringify(query)
-})
-    .then(response => response.json())
-    .then(data => populatePage(data))
-    .catch(console.error)
-
-async function populatePage(data) {
-    document.getElementById('membersPresent').innerHTML = data.membersPresent;
+function populatePage(dataArray) {
+	let data = dataArray[0]
+  console.log(data)
+  document.getElementById('membersPresent').innerHTML = data.membersPresent;
     document.getElementById('clientName').innerHTML = data.clientName;
     document.getElementById('date').innerHTML = `${data.month}/${data.day}/${data.year}`;
-    document.getElementById('time').innerHTML = `${data.start} ${data.AMPM} - ${data.end} ${AMPM2}` ;
+    document.getElementById('time').innerHTML = `${data.start} ${data.AMPM} - ${data.end} ${data.AMPM2}` ;
 
 //convert to arrays 
     let goal = [data.goal1, data.goal2, data.goal3]
@@ -39,28 +44,30 @@ async function populatePage(data) {
     
 //populate using arrays
     for (let i = 1; i < 4; i++) {
-        document.getElementById(`goal${i}`).innerHTML = data.goal[i];
-        document.getElementById(`parentalStrategies${i}`).innerHTML = data.parentalStrategies[i];
-        document.getElementById(`nextSteps${i}`).innerHTML = data.nextSteps[i];
-    }
-
-    document.getElementById('staffName').innerHTML = data.staffName;
-    showPage()
-}
-
-function showPage() {
-    document.getElementById('loadingAnimationSection').style.display = "none";
-    for (let i = 1; i < 5; i++) {
-       document.getElementById(`mainSection${1}`).style.display = "none";
+        document.getElementById(`goal${i}`).innerHTML = goal[i-1];
+        document.getElementById(`parentalStrategies${i}`).innerHTML = parentalStrategies[i-1];
+        document.getElementById(`nextStep${i}`).innerHTML = nextSteps[i-1];
     }
 }
 
-let printToPDF = document.getElementById('printToPDF')
-printToPDF.addEventListener('click', (e) = {})
+printForm.addEventListener('click', (e) => {window.print()})
+let upload = document.getElementById('upload')
+upload.addEventListener('click', (e) => {document.getElementById('uploadEmbed').style.display = 'inline';})
 
-let exit = document.getElementById('exit') 
-exit.addEventListener('click', (e) => {
-    history.back()
-})
 
-//send notices to 3
+async function uploadFile() {
+  let formData = new FormData(); 
+  formData.append("fileupload", fileupload.files[0]);
+    await fetch('http://localhost/upload', {
+    method: "POST", 
+    body: formData
+    })
+        .then(response => response.json())    
+        .then(data => handleResponse(data))
+        .catch(console.error); 
+}
+
+
+async function handleResponse(data) { 
+    //send notices to 3
+}
